05c466a10b3e0502072468de6785e11d8701190b
Junior Alecrim <92541251+junioralecrim@users.noreply.github.com>
2022-02-14 15:10:53 -0300

    Create Idade média homens, mulheres e grupo.py

---
+++ Exercícios/Exercícios incompletos/Idade média homens, mulheres e grupo.py
@@ -0,0 +1,13 @@
+listaGeral = []
+listaHomens = []
+listaMulheres = []
+
+for i in range(1):
+    idade = input("Digite sua idade: ")
+    sexo = input("Digite seu sexo ('M' para masculino e 'F' para feminino): ")
+    sexo = sexo.upper()
+
+    if (sexo == 'M'):
+        
+
+    
