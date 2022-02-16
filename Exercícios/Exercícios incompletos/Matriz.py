74773b82f38f18fd558305f93293dd71dedebf69
Junior Alecrim <92541251+junioralecrim@users.noreply.github.com>
2022-02-16 14:47:53 -0300

    Consegui armazenar os elementos de forma ordenada, porém...
    ainda não consigo exibir eles na lista de forma separada.

--- Exercícios/Exercícios incompletos/Matriz.py
+++ Exercícios/Exercícios incompletos/Matriz.py
@@ -1,14 +1,19 @@
 contPosição = 0
 mat = [[0 for _ in range(3)]for _ in range(3)]
 matOrdenado = [[0 for _ in range(3)]for _ in range(3)]
+aux = []
 
 for i in range(3):
     for j in range(3):
         mat[i][j] = input("Digite o valor %d°: " % (contPosição+1))
         contPosição += +1
 
-        if(contPosição == 3):
-            matOrdenado = sorted(mat)
+        if(contPosição <= 3):
+            aux += mat[i][j]
+            matOrdenado = sorted(aux)
+        if((contPosição > 3) and (contPosição <= 6)):
+            aux += mat[i][j]
+            matOrdenado = sorted(aux)
 
 
 print(matOrdenado)
