66851547411be7d2240c3a3929041692fe78386a
Junior Alecrim <92541251+junioralecrim@users.noreply.github.com>
2022-02-17 15:59:37 -0300

    Update Matriz.py

--- Exercícios/Exercícios incompletos/Matriz.py
+++ Exercícios/Exercícios incompletos/Matriz.py
@@ -3,54 +3,52 @@ lista2 = []
 lista3 = []
 listaAux = []
 cont = 0
-cont2 = 0
 
 for i in range(9):
     if(cont < 3):
         e = input("Digite o %d° valor: " % (cont + 1))
-        lista1.append(e)
         listaAux.append(e)
         cont += +1
         
 
     if((cont >= 3) and (cont < 6)):
         e = input("Digite o %d° valor: " % (cont + 1))
-        lista2.append(e)
         listaAux.append(e)
         cont += +1
 
 
     if((cont >= 6) and (cont < 9)):
         e = input("Digite o %d° valor: " % (cont + 1))
-        lista3.append(e)
-        listaAux.append(e)
-        listaAux = sorted(listaAux)
-        
+        listaAux.append(e)        
         cont += +1
 
-while (cont2 < 9):
+listaAux.sort(key=len)
+cont = 0
+
+while (cont < 9):
   
-  cont2 += +1
+    if(cont < 3):
+        lista1.append(listaAux[cont])
+    
+    if((cont >= 3) and (cont < 6)):
+        lista2.append(listaAux[cont])
 
-  if(cont2 < 3):
-    lista1.append(listaAux[+1])
-  
-  if((cont >= 6) and (cont < 9)):
-    lista2 = listaAux[-3]
+    if((cont >= 6) and (cont < 9)):
+        lista3.append(listaAux[cont])
 
-
-
-
-
+    cont += +1
 
 
 
 
-matriz = [lista1] + [lista2] 
+
+
+
+matriz = [lista1] + [lista2] + [lista3]
 
 #       
 
-print(lista1)
+print(listaAux)
 
 # https://pt.stackoverflow.com/questions/91908/como-obter-a-linha-de-um-determinado-indice-de-matriz-python
 
