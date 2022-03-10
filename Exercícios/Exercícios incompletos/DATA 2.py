82827255f0e0d24c2f3c5f470c1eefd8b9bf1156
Junior Alecrim <92541251+junioralecrim@users.noreply.github.com>
2022-03-10 12:29:44 -0300

    Terminada a parte 1 da questão da data
    A parte de verificação sem os anos bissextos foi terminada. Agora falta incluir os anos bissextos por verificação lógica

--- Exercícios/Exercícios incompletos/DATA 2.py
+++ Exercícios/Exercícios incompletos/DATA 2.py
@@ -4,6 +4,7 @@
 i = 1
 contadorDias_1 = 0
 contadorDias_2 = 0
+contadorDiasInicial = 0
  
 data1_Dia = int(input("Digite o dia em que você nasceu: "))
 data1_Mes = int(input("Digite o mes em que você nasceu: "))
@@ -35,7 +36,8 @@ while (i < data2_Mes):
 
 contadorDias_1 += data2_Dia
 
-###########
+## ANO QUE A PESSOA DIGITOU INICIALMENTE ##
+# Contando a quantidade de dias do mês seguinte ao digitado inicialmente até o final do ano
 i = (data1_Mes + 1)
 
 while (i <= 12):
@@ -51,12 +53,27 @@ while (i <= 12):
     
     i += 1
 
-##FALTA SÓ PEGAR O MÊS EXATO EM QUE SE ESTÁ, POIS ESTOU EXCLUINDO ELE POR COMPLETO E PRECISO TIRAR APENAS OS DIAS A PARTIR DA DATA QUE DIGITEI
-contadorDias_2 = contadorDias_2 + (data1_Dia)
+i = data1_Mes
+# Contando os dias desde a data do dia inicial digitado até o final do mês 1
 
-#teoricamente esta certo
+if((i == 1) or (i == 3) or (i == 5) or (i == 7) or (i == 8) or (i == 10) or (i == 12)):
+        contadorDiasInicial += 31 - data1_Dia     
+
+if((i == 4) or (i == 6) or (i == 9) or (i == 11)):
+        contadorDiasInicial += 31 - data1_Dia
+
+if(i == 2):
+        contadorDiasInicial += 31 - data1_Dia
+
+#Contador de dias a partir da data em que o usuário digitou inicialmente.
+
+# VERIFICAÇÃO 
+
+
+
+contadorDias_2 = contadorDias_2 + contadorDiasInicial
 
 totalDias = (contadorDias_1 + contadorDias_2) + (anos*365)
 
 print(f"Anos de vida {anos}")
-print(f"A quantidade de dias entre a data digitada e a data atual é: {contadorDias_2}")
+print(f"A quantidade de dias entre a data digitada e a data atual é: {totalDias}")
