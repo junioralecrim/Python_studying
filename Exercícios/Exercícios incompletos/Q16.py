bf24dbd9d15c35846c081f06a66995ff198dd1bf
Junior Alecrim <92541251+junioralecrim@users.noreply.github.com>
2022-03-14 21:08:57 -0300

    Exercício concluído

--- Exercícios/Exercícios incompletos/Q16.py
+++ Exercícios/Exercícios incompletos/Q16.py
@@ -1,19 +1,22 @@
 print("----- QUANTIDADE DE LATAS DE TINTA E VALOR TOTAL -----")
-area = float(input("Digite a área total a ser pintada em metros: "))
+area = float(input("Digite a área total a ser pintada em metros quadrados: "))
 
 #divide a quantidade de metros dados por 3, que é a quantidade de litros pra cada 3m (1:3)
 quantLitros = area/3
 i = 0
 contadorLitros = quantLitros
+contadorLatas = 0
 
 #trabalhando no caso para caso seja 1 ou maior o número de latas
 while (contadorLitros > 0):
     if(quantLitros >= 18):
         i += 1
         contadorLitros += - 18
+        contadorLatas += 1
     else:
         contadorLitros += - 18
+        contadorLatas += 1
 
-contadorLatas = i
 
-print(f"Verificando o contador de litros: {contadorLitros}\nVerificando o contador de latas: {contadorLatas}\nVerificando a quantidade de listros: {quantLitros}")
+
+print(f"\nVocê vai precisar de exatamente %.2f litros de tinta\n\nA quantidade de latas mínimas para a pintura é de {contadorLatas} lata" % quantLitros)
