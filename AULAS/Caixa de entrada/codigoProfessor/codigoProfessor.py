c7617728671f48e2eb814121a57d87ffa60ef8b5
Junior Alecrim <92541251+junioralecrim@users.noreply.github.com>
2022-04-25 12:15:09 -0300

    up

---
+++ AULAS/Caixa de entrada/codigoProfessor/codigoProfessor.py
@@ -0,0 +1,36 @@
+from openpyxl import load_workbook
+
+#https://letscode.com.br/blog/aprenda-a-integrar-python-e-excel
+#https://openpyxl.readthedocs.io/en/stable/tutorial.html
+
+#caminho = 'AULAS-EAD-REGISTRO-DE-ATIVIDADE.xlsx'
+caminho = 'Aprendendoalerplanilhas.xlsx'
+arquivo_excel = load_workbook(caminho, data_only=True)
+
+planilha = arquivo_excel.active
+
+
+#planilha.cell(row=7, column=4, value='Teste euris')
+#arquivo_excel.save("Aprendendoalerplanilhas.xlsx")
+alunos = []
+for i in range(7, 17):
+    conteudo = planilha[f'B{i}']
+    alunos.append(conteudo.value)
+
+resultadoAlunosSim = {}
+resultadoAlunosNao = {}
+
+for a in alunos:
+    somasim = 0
+    somanao = 0
+    for i in range(7, 36):
+        celulaAluno = planilha[f'B{i}']
+        celulaResposta = planilha[f'C{i}']
+        if celulaAluno.value == a and celulaResposta.value == 'SIM':
+            somasim += 1
+            resultadoAlunosSim[a] = somasim
+        elif celulaAluno.value == a and celulaResposta.value == 'NÃO':
+            somanao += 1
+            resultadoAlunosNao[a] = somanao
+print("SIM", resultadoAlunosSim)
+print("NÃO", resultadoAlunosNao)
