6126ebdaca1b2580ad9e6a8c0fc94b939a9ba3f4
Junior Alecrim <junioralecrim.16@gmail.com>
2022-05-11 16:17:14 -0300

    Update selectionSort.py

--- Exercícios/Exercícios completo/Faculdade/Exercícios/Revisão Prova/Q6 - Considere um vetor-Aplique a técnica de ordenação do selection sort/selectionSort.py
+++ Exercícios/Exercícios completo/Faculdade/Exercícios/Revisão Prova/Q6 - Considere um vetor-Aplique a técnica de ordenação do selection sort/selectionSort.py
@@ -14,3 +14,16 @@ for i in range(len(v)):
            print(f"Aqui precisa estar printado somente o 41: {aux}")
 
 print(maxim)
+
+
+
+
+def sort(array):
+    for index in range(0, len(array)):
+        min_index = index
+
+        for right in range(index + 1, len(array)):
+            if array[right] < array[min_index]:
+                min_index = right
+
+        array[index], array[min_index] = array[min_index], array[index]
