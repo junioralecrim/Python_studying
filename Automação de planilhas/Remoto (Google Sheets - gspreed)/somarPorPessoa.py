ab53b462606102b779411a92cc1a1097c098a0bc
Junior Alecrim <junioralecrim.16@gmail.com>
2022-05-30 14:46:53 -0300

    Update somarPorPessoa.py

--- Automação de planilhas/Remoto (Google Sheets - gspreed)/somarPorPessoa.py
+++ Automação de planilhas/Remoto (Google Sheets - gspreed)/somarPorPessoa.py
@@ -5,21 +5,21 @@ def tratamentoString(linha, coluna):
     celula = int(celula[3:(len(celula)) - 3])
     return celula
 
-def tratamentoLista(lista, valor): #essa função serve para capturar os valores, só que ela precisa que tenha algum valor dentro da lista ja
+def tratamentoLista(lista, valor): #essa função serve para capturar os valores, só que ela precisa que a lista não esteja vazia.
     for i in range(len(lista)):
-        if(valor == lista[i]):
+        if (valor == lista[i]):
             return lista
         else:
             if (i == (len(lista) - 1)):
                 lista.append(valor)
                 return lista
 
-pessoaArthur = 0
-pessoaAlice = 0
-pessoaMatheus = 0
+
+aux = 0
+comissaoPessoas = [0, 0, 0]
 pessoas = [0] #usando um dicionário pra colocar o nome das pessoas
 pessoasT = ('arthur', 'matheus', 'alice')
-colunas = ('vendedor')
+colunas = 'vendedor'
 
 #essa parte é a minha chave de acesso do google drive à API do google sheets
 gc = gspread.service_account(filename='api.json')
@@ -47,21 +47,25 @@ for k in range(1, 9): #fazendo a busca da célula dos vendedores para capturar o
             ##print('\n')
             ##print(f"Coletando nomes dos vendedores de forma unificada - {tituloCelula}")
 
-            if(pessoas[0] == 0): #essa parte vai servir pra adicionar o primeiro nome sempre.
-                pessoas[0] = tituloCelula
-            pessoas = tratamentoLista(pessoas, tituloCelula) #função para eliminar repetições de nomes nas lista de vendedores
+            #if(pessoas[0] == 0): #essa parte vai servir pra adicionar o primeiro nome sempre.
+            #    pessoas[0] = tituloCelula
+            #pessoas = tratamentoLista(pessoas, tituloCelula) #função para eliminar repetições de nomes nas lista de vendedores
             #print(f'Unificando lista de vendedores: {pessoas}')
 
             if(tituloCelula == pessoasT[0]): #pra passar isso pra função, eu vou precisar repassar os valores de i, k
-                pessoaArthur += tratamentoString(i, k)
+                aux = tratamentoString(i, k)
+                comissaoPessoas[0] = comissaoPessoas[0] + aux
 
             if (tituloCelula == pessoasT[1]):
-                pessoaMatheus += tratamentoString(i, k)
+                aux = tratamentoString(i, k)
+                comissaoPessoas[1] = comissaoPessoas[1] + aux
 
             if (tituloCelula == pessoasT[2]):
-                pessoaAlice += tratamentoString(i, k)
+                aux = tratamentoString(i, k)
+                comissaoPessoas[2] = comissaoPessoas[2] + aux
 
-print(f"\n\nArthur: {pessoaArthur}\nMatheus: {pessoaMatheus}\nAlice: {pessoaAlice}")
-
+#print(f"\n\nArthur: {pessoaArthur}\nMatheus: {pessoaMatheus}\nAlice: {pessoaAlice}")
+print("PROCESSAENTO DE DADOS CONCLUÍDO\n")
+print(f"\n{pessoasT[0]}: {comissaoPessoas[0]}\n{pessoasT[1]}: {comissaoPessoas[1]}\n{pessoasT[2]}: {comissaoPessoas[2]}")
 
 
