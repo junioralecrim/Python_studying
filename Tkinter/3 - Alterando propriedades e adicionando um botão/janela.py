02274f52ec3a89bcd1060666472566706b62d340
Junior Alecrim <junioralecrim.16@gmail.com>
2022-06-01 15:32:41 -0300

    Update janela.py

--- Tkinter/3 - Alterando propriedades e adicionando um botão/janela.py
+++ Tkinter/3 - Alterando propriedades e adicionando um botão/janela.py
@@ -1,14 +1,13 @@
 from tkinter import *
 
 menu_inicial = Tk()
-menu_inicial.title("Primeira aplicação")
+menu_inicial.title("Aplicação")
 
 menu_inicial.geometry("500x400+200+200")
-
-menu_inicial.resizable(True, True)
-
 menu_inicial.minsize(width=500, height=400)
+menu_inicial.iconbitmap("imagens/icon.ico")
 
-menu_inicial.iconbitmap("imagens/icon.ico") 
+menu_inicial['bg'] = "blue" #serve para definir a cor de fundo da janela principal
 
-menu_inicial.mainloop() 
+
+menu_inicial.mainloop()
