ce2a8973c871da1e24794066d7e3a370ba029ecb
Junior Alecrim <junioralecrim.16@gmail.com>
2022-06-01 15:44:14 -0300

    Estudos-Tkinter
    Aprendi a adicionar o botão na janela principal e a mudar a cor do bg

--- Tkinter/3 - Alterando propriedades e adicionando um botão/janela.py
+++ Tkinter/3 - Alterando propriedades e adicionando um botão/janela.py
@@ -7,7 +7,12 @@ menu_inicial.geometry("500x400+200+200")
 menu_inicial.minsize(width=500, height=400)
 menu_inicial.iconbitmap("imagens/icon.ico")
 
-menu_inicial['bg'] = "blue" #serve para definir a cor de fundo da janela principal
+menu_inicial['bg'] = "grey" #serve para definir a cor de fundo da janela principal
+
+#botao
+
+botao = Button(menu_inicial, text="Executar") #Button(a onde o botão pertence, texto no botão)
+botao.pack() #adicionando o botão à janela
 
 
 menu_inicial.mainloop()
