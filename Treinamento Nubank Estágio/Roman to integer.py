b06e15700bbe67d6b05739520cd3ad8489c210c5
Junior Alecrim <junioralecrim.16@gmail.com>
2022-06-12 16:08:22 -0300

    Update Roman to integer.py

--- Treinamento Nubank Estágio/Roman to integer.py
+++ Treinamento Nubank Estágio/Roman to integer.py
@@ -1,13 +1,12 @@
 num = (input("Digite um número romano qualquer para obter seu equivalente numérico: "))
 num = num.upper()
-soma = 0
 aux = 0
 
 romanNumber = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 for i in range(len(num)):
-    aux = i + 1
-    # verificando se o se a letra existe no dicionário
-    if num[i] in romanNumber: #se existir, o código continua     if num[i] in romanNumber: #se existir, o código continua
-        if romanNumber[num[i]] >= romanNumber[num[aux]] and i < len(num) - 1:
-            print(i)
+    #procurar número de ocorrências das letras i, x, c e m. Só pode ser maior ou igual a 3.
+    aux += romanNumber[num[i]]
+
+
+print(soma)
