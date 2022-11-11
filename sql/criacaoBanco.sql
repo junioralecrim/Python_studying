fd42cafd16b81c4bfdfe9f4d7d78faf87b53cd6b
Junior Alecrim <92541251+junioralecrim@users.noreply.github.com>
2022-11-11 19:57:58 -0300

    Create criacaoBanco.sql

---
+++ sql/criacaoBanco.sql
@@ -0,0 +1,21 @@
+
+
+create table pet(
+	id int not null auto_increment,
+    id_resp int,
+    nome varchar(10),
+    tipo varchar(10),
+    raca varchar(10) not null,
+    primary key(id),
+    foreign key(id_resp) references responsavel(id)
+);
+
+create table responsavel(
+ 	id int not null auto_increment,
+    nome varchar(30),
+    cpf varchar(11) not null,
+    telefone varchar(15),
+    primary key(id)
+);
+
+select * from responsavel;
